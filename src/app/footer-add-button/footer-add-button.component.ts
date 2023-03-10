91e2d95b41b15e87fb8391f2d645e2cc0f4fd973
Mike Meier <m0.0m58@yahoo.de>
2023-03-10 22:37:57 +0100

    change from scss to css

--- src/app/footer-add-button/footer-add-button.component.ts
+++ src/app/footer-add-button/footer-add-button.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer-add-button',
   templateUrl: './footer-add-button.component.html',
-  styleUrls: ['./footer-add-button.component.scss']
+  styleUrls: ['./footer-add-button.component.css']
 })
 export class FooterAddButtonComponent {
 
